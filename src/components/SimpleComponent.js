dfc3113847317577025f2457e473037bdd3143ff
tylergreason <tyler.greason@protonmail.com>
2020-03-13 07:44:20 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,22 @@
-// Code SimpleComponent Here
+import React, { Component } from 'react'
+export default class SimpleComponent extends Component {
+    constructor(){
+        super();
+        this.state = {
+            mood:'happy'
+        }
+    }
+    
+    handleClick = (e) => {
+        return (this.state.mood === 'happy' ? this.setState({mood:"sad"}) : this.setState({mood:"happy"}))
+    }
+    render(){
+        return(
+            <div
+            onClick={this.handleClick}
+            >
+                {this.state.mood}
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+function consoleLog(){
+  console.log('this is a console log on click')
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={consoleLog} />
   </div>,
   document.getElementById('root')
 );
